581aaebe02702149cc65a99a14ac79b77cf45830
Nicholas D. Socci <soccin@mskcc.org>
2017-07-05 19:45:37 -0400

    exported SNAME variable

--- execWrapper.sh
+++ execWrapper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 export SDIR="$( cd "$( dirname "$0" )" && pwd )"
+export SNAME=$(basename $0)
 export R_LIBS=$SDIR/Rlib:$R_LIBS
 
-DOCFILE=$SDIR/docs/$(basename $0).doc
+DOCFILE=$SDIR/docs/${SNAME}.doc
 if [ "$#" == "0" ] && [ -e $DOCFILE ]; then
     # If no args then just print usage
     # do not bother running R script
