2f60e12796940f73d57238ff74f4a14e0daa77be
Nicholas D. Socci <soccin@mskcc.org>
2017-07-12 22:13:31 -0400

    Added undo.SD to param var for output

--- seqSegment.R
+++ seqSegment.R
@@ -63,12 +63,14 @@ if(args$BINSIZE=="auto") {
     cat("# manually set binsize =",binSize,"\n")
 }
 
-out=seqsegment(bb,sampleid=sampleId,binSize=binSize)
+undo.SD=2
+out=seqsegment(bb,sampleid=sampleId,binSize=binSize, undo.splits="sdundo", undo.SD=undo.SD)
 out$param=list()
 out$param$binSize=binSize
 out$param$sampleId=sampleId
 out$param$countFile=args$COUNTS
 out$param$arg.binsize=args$BINSIZE
+out$param$undo=list(undo.splits="sdundo",undo.SD=undo.SD)
 
 #
 # probe.seg.values is the segment means projected
@@ -85,7 +87,9 @@ numSegments=nrow(out$output)
 
 png(file=file.path(args$ODIR,cc(sampleId,"seqSeg",".png")),
         type="cairo",
-        width=1150,height=800,pointsize=20)
+        height=1150,width=800,pointsize=16)
+
+par(mfrow=c(2,1))
 
 YLIM=3
 plot(out,xmaploc=F,ylim=YLIM*c(-1,1),pt.cols=c("#B5D7E4","#BEBEBE"))
@@ -103,16 +107,19 @@ text(0.5,YLIM+.5-1,
         formatC(global.mad,format="f"),
         "NumSegs =",formatC(numSegments,format="d",big.mark=",")
         ),
-    pos=4,cex=1.2)
+    pos=4,cex=1.12)
 
-text(0.5,YLIM+.5-1.25,
+text(0.5,YLIM+.5-1.4,
     paste(
         "max|segMean| Auto =",
         formatC(max(abs(out$output$seg.mean)),format="f"),
         "RMSD(seg.mean) =",
         formatC(sqrt(mean(out$output$seg.mean^2)),format="f")
         ),
-    pos=4,cex=1.2)
+    pos=4,cex=1.12)
+
+plot(out,xmaploc=F,pt.cols=c("#B5D7E4","#BEBEBE"))
+abline(h=c(-2,2),lty=2,col="#333333",lwd=1)
 
 dev.off()
 
@@ -145,6 +152,7 @@ writeVariable("arg.binsize")
 writeVariable("binSize")
 numBins=nrow(out$data)
 writeVariable("numBins")
+writeVariable("undo.SD")
 writeVariable("global.mad")
 writeVariable("rms.derivative.noise")
 numSegments=nrow(output)
@@ -192,6 +200,7 @@ writeVariable("X.seg.mean.avg")
 # nsegs=nrow(segs)
 
 # cnlr=out$data[is.finite(out$data[,3]),3]
+# weights=out$weights[is.finite(out$data[,3])]
 
 # segid <- rep(rank(segs$seg.mean, ties.method="random"), segs$num.mark)
 # segid <- sort(segid)
@@ -201,10 +210,16 @@ writeVariable("X.seg.mean.avg")
 # ocnclust<-1:nsegs
 # ocnlevels<-segs$seg.mean
 
-#     while ((length(ocnlevels) > 1) && (min(diff(ocnlevels)) < 0.04)) {
+# # DMP uses threshold=0.1
+#     threshold=0.04
+#     while ((length(ocnlevels) > 1) && (min(diff(ocnlevels)) < threshold)) {
 #         j <- which.min(diff(ocnlevels))
 #         ocnlevels <- ocnlevels[-j]
 #         ocnclust[ocnclust>j] <- ocnclust[ocnclust>j] - 1
 #         segid[segid > j] <- segid[segid > j] - 1
-#         ocnlevels[j] <- median(cnlr[segid==j])
+#         wj=weights[segid==j]/sum(weights[segid==j])
+#         ocnlevels[j] <- mean(wj*cnlr[segid==j])
 #     }
+
+# out$output$cluster=ocnclust[rank(out$output$seg.mean,ties.method="random")]
+# out$output$clust.mean=round(ocnlevels[out$output$cluster],4)
