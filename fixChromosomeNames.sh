587132a13780aafbec77d96a431d37dd049ed20d
Nicholas D. Socci <soccin@mskcc.org>
2018-02-22 19:48:22 -0500

    Added script to reheader BAMs to 1,2,3 convention (from chr1, chr2, ...)

---
+++ fixChromosomeNames.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+IBAM=$1
+BASE=$(basename $IBAM | sed 's/.bam//')
+OBAM=${BASE}__relabel.bam
+ODIR=bamRelabel/${BASE}
+
+mkdir -p $ODIR
+samtools view -H $IBAM | egrep "^@(SQ|HD)" | sed 's/SN:chr/SN:/' >$ODIR/${BASE}.relabel.dict
+samtools reheader $ODIR/${BASE}.relabel.dict $IBAM >$ODIR/$OBAM
