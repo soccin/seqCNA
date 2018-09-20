0adcd1624b7d910e064800865efd2a6b0f3e46a7
Nicholas D. Socci <soccin@mskcc.org>
2018-09-19 21:36:41 -0400

    Index relabled bam

--- fixChromosomeNames.sh
+++ fixChromosomeNames.sh
@@ -10,3 +10,4 @@ ODIR=bamRelabel/${BASE}
 mkdir -p $ODIR
 samtools view -H $IBAM | egrep "^@(SQ|HD)" | sed 's/SN:chr/SN:/' >$ODIR/${BASE}.relabel.dict
 samtools reheader $ODIR/${BASE}.relabel.dict $IBAM >$ODIR/$OBAM
+samtools index $ODIR/$OBAM
