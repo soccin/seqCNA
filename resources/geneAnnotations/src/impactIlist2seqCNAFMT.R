e51a50082b76db0711d5739bdd7228f3cdee6717
Nicholas D. Socci <soccin@mskcc.org>
2018-10-21 14:21:54 -0400

    Added M-IMPACT_v1 assay def

---
+++ resources/geneAnnotations/src/impactIlist2seqCNAFMT.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+impactIlist="/home/socci/Work/CMO/Mouse_IMPACT/BICTargetFiles/M-IMPACT_v1_mm10/M-IMPACT_v1_mm10_targets.ilist"
+
+header=readLines(impactIlist,1000)
+header=grep("^@",header,value=T)
+if(len(header)==1000) {
+    stop("Need bigger cache")
+}
+
+ann <- read_tsv(impactIlist,col_names=F,comment="@") %>%
+    filter(grepl(":ENSMUST",X5)) %>%
+    separate(X5,c("gene","transcript"),sep=":") %>%
+    arrange(transcript,X2) %>%
+    group_by(gene,transcript) %>%
+    mutate(exon=paste0("exon",row_number())) %>%
+    rename(chrom=X1,start=X2,stop=X3) %>%
+    select(chrom,start,stop,gene,transcript,exon) %>%
+    ungroup %>%
+    mutate(chrom=factor(gsub("^chr","",chrom),levels=c(1:99,"X","Y","M","MT"))) %>%
+    arrange(chrom,start)
+
