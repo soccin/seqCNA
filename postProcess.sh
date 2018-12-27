9752e878094704cfe3ca24b7473e31d8f8785cfe
Nicholas D. Socci <soccin@mskcc.org>
2018-12-27 17:37:35 -0500

    Fix post to check for valid assay

--- postProcess.sh
+++ postProcess.sh
@@ -11,6 +11,7 @@ if [ "$#" -lt "1" ]; then
     exit
 fi
 
+
 if [ "$#" -ge "2" ]; then
     projectName=$2
 else
@@ -18,6 +19,15 @@ else
 fi
 
 assay=$1
+if [ ! -e "$SDIR/resources/geneAnnotations/$assay" ]; then
+    echo "usage: postProcess.sh AssayName [ProjectName]"
+    echo ""
+    echo "[$assay] is not a valid assay name"
+    echo
+    ls -1 $SDIR/resources/geneAnnotations | fgrep -v src | awk '{print "   "$1}'
+    echo
+    exit
+fi
 
 mkdir $projectName
 find out | fgrep .png | xargs -I % cp % $projectName
@@ -25,5 +35,6 @@ find out | fgrep .seg | head -1  | xargs head -1 | cut -f-6 >$projectName/${proj
 find out | fgrep .seg | xargs cut -f-6 | fgrep -v "loc.start" >>$projectName/${projectName}___IGV.seg
 
 find out -type f  | fgrep .seg | perl -pe 's|/[^/]+$|\n|' >lodir
+
 $SDIR/getGeneCalls ASSAY=$assay INPUTS=lodir ODIR=$projectName
 
