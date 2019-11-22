77e720ae7043d87e3be5a542d72471faec12d23f
Nicholas D. Socci <soccin@mskcc.org>
2019-11-22 13:31:25 -0500

    Check if fixBam already exists

--- fixChromosomeNames.sh
+++ fixChromosomeNames.sh
@@ -8,6 +8,12 @@ OBAM=${BASE}__relabel.bam
 ODIR=bamRelabel/${BASE}
 
 mkdir -p $ODIR
+
+if [ -e $ODIR/$OBAM ]; then
+    echo "$ODIR/$OBAM already exists, reusing"
+    exit
+fi
+
 samtools view -H $IBAM | egrep "^@(SQ|HD|RG|PG)" | sed 's/SN:chr/SN:/' >$ODIR/${BASE}.relabel.dict
 samtools reheader $ODIR/${BASE}.relabel.dict $IBAM >$ODIR/$OBAM
 samtools index $ODIR/$OBAM
