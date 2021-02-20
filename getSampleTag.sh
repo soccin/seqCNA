17f7af92ebe797980d8da18c47cf5eb083192ddb
Nicholas D. Socci <soccin@mskcc.org>
2021-02-20 15:17:35 -0500

    Normalize sampleId's: Add s_ if it does not already have it

--- getSampleTag.sh
+++ getSampleTag.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-samtools view -H $1 \
+SID=$(samtools view -H $1 \
     | egrep "^@RG" \
     | tr '\t' '\n' \
     | fgrep SM: \
     | sed 's/SM://' \
     | uniq \
     | xargs \
-    | tr ' ' ','
+    | tr ' ' ',')
+
+if [[ ! $SID =~ ^s_ ]]; then
+    echo "s_"$SID
+else
+    echo $SID
+fi
 
