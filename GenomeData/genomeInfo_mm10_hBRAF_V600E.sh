7104d0b46bb014f68ca41e5830b23e7e207df6e4
Nicholas D. Socci <soccin@mskcc.org>
2022-07-07 17:39:20 -0400

    Old version not used; moved to attic

--- GenomeData/genomeInfo_mm10_hBRAF_V600E.sh
+++
@@ -1,6 +0,0 @@
-# Need to use the version which has lower case ==> upper case
-# to fix issue with VEP
-GENOME=/ifs/work/socci/Depot/Genomes/M.musculus/ucsc/mm10/mm10_Fixed.fasta
-GENOMEFAI=/ifs/work/socci/Depot/Genomes/M.musculus/ucsc/mm10/mm10_Fixed.fasta.fai
-EXACDB=""
-FACETSNPS=/home/socci/Code/Pipelines/FACETS/FACETS.app/lib/mouse_MM10_dbSNP_NCBI_20150625___noChrM__plusPseudo50__DROP_SORT.vcf.gz
