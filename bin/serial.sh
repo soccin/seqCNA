976675146f2333630424463e1c5406c528a2afce
Nicholas D. Socci <soccin@mskcc.org>
2022-07-07 16:20:11 -0400

    Serial version of QRUN system

--- bin/serial.sh
+++ bin/serial.sh
@@ -2,7 +2,7 @@ QSYNC=#
 
 
 ##
-# QRUN ALLOC QTAG <HOLD hold_id> <VMEM size> LONG
+# QRUN NCORES QTAG <HOLD hold_id> <VMEM size> LONG
 #
 # HOLD, VMEM and LONG are optional but if you more than one
 # they must be in this order
@@ -13,33 +13,7 @@ QSYNC=#
 
 QRUN () {
 
-    if [ "$LSF_VERSION" == "" ]; then
-        export LSF_VERSION=$(echo $LSF_SERVERDIR | perl -ne 'm|/([^/]+)/linux|;print $1')
-        echo setting LSF_VERSION="$LSF_VERSION"
-    fi
-
-    case $LSF_VERSION in
-        10.1)
-            TIME_FLAG="-W"
-            TIME_SHORT="$TIME_FLAG 59"
-            TIME_LONG="$TIME_FLAG 359"
-
-        ;;
-
-        9.1)
-            TIME_FLAG=""
-            TIME_SHORT=""
-            TIME_LONG=""
-        ;;
-
-        *)
-        echo "Error invalid LSF_VERSION ["${LSF_VERSION}"]"
-        exit -1
-        ;;
-
-    esac
-
-    ALLOC=$1
+    NCORES=$1
     QTAG=$2
     echo QTAG=$QTAG
     shift 2
@@ -52,18 +26,14 @@ QRUN () {
 
     VMEM=""
     if [ "$1" == "VMEM" ]; then
-        if [ "$LSF_VERSION" == "10.1" ]; then
 
-            TOTALMEM=$2
-            MEMPERSLOT=$((TOTALMEM / ALLOC))
-            VMEM='-R "rusage[mem='$MEMPERSLOT']"'
-
-        else
-            VMEM='-R "rusage[mem='$2']"'
-        fi
+        TOTALMEM=$2
+        MEMPERSLOT=$((TOTALMEM / NCORES))
+        VMEM='-R "rusage[mem='$MEMPERSLOT']"'
 
         shift 2
         echo VMEM=$VMEM
+
     fi
 
     TIME=$TIME_SHORT
@@ -73,10 +43,9 @@ QRUN () {
         echo LONG Job
     fi
 
-    RET=$(bsub $TIME $QHOLD $VMEM -n $ALLOC -J $QTAG -o LSF.PEMAP/ $*)
-    echo RET=bsub $QHOLD $VMEM -n $ALLOC -J $QTAG -o LSF.PEMAP/ $*
+    #RET=$(bsub $TIME $QHOLD $VMEM -n $NCORES -J $QTAG -o LSF.PEMAP/ $*)
+    echo RET=bsub $QHOLD $VMEM -n $NCORES -J $QTAG -o LSF.PEMAP/ $*
     echo "#QRUN RET=" $RET
     echo
-    JOBID=$(echo $RET | perl -ne '/Job <(\d+)> /;print $1')
 
 }
